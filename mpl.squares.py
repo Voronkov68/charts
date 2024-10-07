a7ef3f6bc1933eedb4f186992d637f645c15a927
Voronkov68 <158170491+Voronkov68@users.noreply.github.com>
2024-10-07 23:46:59 +0300

    added style: dark_background

--- mpl.squares.py
+++ mpl.squares.py
@@ -1,13 +1,20 @@
 import matplotlib.pyplot as plt 
+import seaborn as sns
+
 
 squares = [1, 4, 9, 16, 25]
+input_falues = list(range(1, 6))
+
+plt.style.use('dark_background')
 fig, ax = plt.subplots()
 
-ax.plot(squares, linewidth=3)
+ax.plot(input_falues, squares, linewidth=3)
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square Of Value", fontsize=14)
-ax.tick_params(axis='both', labelsize=14)
+ax.tick_params(axis='both', labelsize=14, labelcolor='red', rotation=45)
+
 
 plt.show()
 
+print(plt.style.available)
